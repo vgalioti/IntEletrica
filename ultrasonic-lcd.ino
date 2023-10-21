a1e4c963d0254211d7f1542e749cc2fb5e90c2a7
Art-Mat <146115630+Art-Mat@users.noreply.github.com>
2023-10-21 13:20:25 -0300

    Update ultrasonic-lcd.ino

--- ultrasonic-lcd.ino
+++ ultrasonic-lcd.ino
@@ -1,77 +1,8 @@
-//lembrar de tirar a declaração da <Ultrasonic.h> e colocar a convert de microsec para CM
 #include <LiquidCrystal.h>
+#include <Ultrasonic.h>
 
-#define DM 28
-#define INC 71
 #define PINO_ECHO 13
 #define PINO_TRIG 10
-
-class Ultrasonic {
-  public:
-    Ultrasonic(uint8_t sigPin) : Ultrasonic(sigPin, sigPin) {};
-    Ultrasonic(uint8_t trigPin, uint8_t echoPin, unsigned long timeOut = 20000UL);
-    unsigned int read(uint8_t und = DM);
-    unsigned int distanceRead(uint8_t und = DM) __attribute__ ((deprecated ("This method is deprecated, use read() instead.")));
-    void setTimeout(unsigned long timeOut) {timeout = timeOut;}
-    void setMaxDistance(unsigned long dist) {timeout = dist*DM*2;}
-    uint8_t trig;
-    uint8_t echo;
-    boolean threePins = false;
-    unsigned long previousMicros;
-    unsigned long timeout;
-    unsigned int timing();
-};
-
-
-
-Ultrasonic::Ultrasonic(uint8_t trigPin, uint8_t echoPin, unsigned long timeOut) {
-  trig = trigPin;
-  echo = echoPin;
-  threePins = trig == echo ? true : false;
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
-  timeout = timeOut;
-}
-
-unsigned int Ultrasonic::timing() {
-  if (threePins)
-    pinMode(trig, OUTPUT);
-
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  delay(30);
-
-  if (threePins)
-    pinMode(trig, INPUT);
-  
-  previousMicros = micros();
-  while(!digitalRead(echo) && (micros() - previousMicros) <= timeout); // wait for the echo pin HIGH or timeout
-  previousMicros = micros();
-  while(digitalRead(echo)  && (micros() - previousMicros) <= timeout); // wait for the echo pin LOW or timeout
-
-  return micros() - previousMicros; // duration
-}
-
-/*
- * If the unit of measure is not passed as a parameter,
- * sby default, it will return the distance in centimeters.
- * To change the default, replace CM by INC.
- */
-unsigned int Ultrasonic::read(uint8_t und) {
-  return timing() / und / 2;  //distance by divisor
-}
-
-/*
- * This method is too verbal, so, it's deprecated.
- * Use read() instead.
- */
-unsigned int Ultrasonic::distanceRead(uint8_t und) {
-  return read(und);
-}
-
 
 Ultrasonic ultrasonic(PINO_TRIG, PINO_ECHO);
 
@@ -91,7 +22,9 @@ void setup() {
 }
 
 void loop() {
+  float cm_sec;
   long microsec = ultrasonic.timing();
+  cm_sec = ultrasonic.convert(microsec, Ultrasonic::CM)
 
   lcd.setCursor(0, 0);
   
@@ -107,7 +40,7 @@ void loop() {
   int contraste = 100;
   
   for(int i = 0; i < 100; i++) {
-    analogWrite(6, contraste - microsec << 2);
+    analogWrite(6, contraste - cm_sec);
     delay(50);
   }
 
