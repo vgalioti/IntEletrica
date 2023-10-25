4635e702936ed5e39b0f7df28447398495f158c0
Art-Mat <146115630+Art-Mat@users.noreply.github.com>
2023-10-25 14:39:23 -0300

    Update ultrasonic-lcd.ino

--- ultrasonic-lcd.ino
+++ ultrasonic-lcd.ino
@@ -1,6 +1,8 @@
 #include <LiquidCrystal.h>
 #include <Ultrasonic.h>
 
+using namespace std;
+
 #define PINO_ECHO 13
 #define PINO_TRIG 10
 
@@ -9,65 +11,99 @@ Ultrasonic ultrasonic(PINO_TRIG, PINO_ECHO);
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+ISR(TIMER1_COMPA_vect);
+void printAds();
+void menu();
 
+// Variaveis globais
+bool showAds = true;
+int adsId;
+int secondsAtAds = 10;
+int secondsAtMenu = 0;
+int teste = 0;
+
+// Variaveis globais para a TelaLCD
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void setup() {
-
-  // set up the LCD's number of columns and rows:
-
+void setup()
+{
+  // Serial.begin(9600)
+  pinMode(7, OUTPUT);
   lcd.begin(16, 2);
+  analogWrite(6, 0);
   Serial.begin(9600);
+  
+  // Configuração do timer para contar cada segundo com o clock.
+  // OCR1A = ((16*10^6) / (F1 * 1024)) - 1
+
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+  OCR1A = 15624; //para F1 = 1 Hz, OCR1A = 15624
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << CS12) | (1 << CS10); 
+  TIMSK1 |= (1 << OCIE1A);
+  
+  interrupts();
 }
 
-void loop() {
+void loop()
+{  
+  // Código do sensor de proximidade.
+  // Exemplo do timer (pseudocodigo)
+  // if(muitoperto) {
+  //    showAds = false;
+  //    menu();
+  //}
   float cm_sec;
   long microsec = ultrasonic.timing();
-  cm_sec = ultrasonic.convert(microsec, Ultrasonic::CM)
+  cm_sec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  if (cm_sec > 50)
+    cm_sec = 50;
+  analogWrite(6, (5 * cm_sec));
+}
 
-  lcd.setCursor(0, 0);
-  
+// Imprime um novo anuncio na tela a cada 10 segundos.
+// Se passar mais de 15 segundos sem interacao no menu, volta a exibir anuncios
+ISR(TIMER1_COMPA_vect) {
+  if(showAds) {
+    secondsAtMenu = 0;
+    secondsAtAds++;
 
-  for (int thisChar = 0; thisChar < 10; thisChar++) {
+    if(secondsAtAds >= 10) {
+      printAds();
+      secondsAtAds = 0;
+    }
+  }
+  else {
+    secondsAtAds = 0;
+    secondsAtMenu++;
+    lcd.clear();
+    if(secondsAtMenu > 15) {
+      showAds = true;
+      secondsAtAds = 10;
+    }
+  }
+}
 
-    lcd.print(thisChar);
-
-    delay(500);
-
+void printAds() {
+  if(teste == 0) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("POLI FINANCE");
+    lcd.setCursor(0, 1);
+    lcd.print("PS 2024 aberto!");
+    teste++;
   }
 
-  int contraste = 100;
-  
-  for(int i = 0; i < 100; i++) {
-    analogWrite(6, contraste - cm_sec);
-    delay(50);
+  else if(teste == 1) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("POLI JUNIOR");
+    lcd.setCursor(0, 1);
+    lcd.print("Maior empresa JR");
+    teste = 0;
   }
-
-  // set the cursor to (16,1):
-
-  lcd.setCursor(16, 1);
-
-  // set the display to automatically scroll:
-
-  lcd.autoscroll();
-
-  // print from 0 to 9:
-
-  for (int thisChar = 0; thisChar < 10; thisChar++) {
-
-    lcd.print(thisChar);
-
-    delay(500);
-
-  }
-
-  lcd.noAutoscroll();
-  
-  for(int i = 100; i > 0; i--) {
-    analogWrite(6, contraste - i);
-    delay(50);
-  }
-
-  lcd.clear();
 }
